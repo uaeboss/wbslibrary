093fb37a8fdf26f7b54a82eb25d95649ffc9ffc8
uaeboss <jeskemal@gmail.com>
2023-07-28 13:40:54 +0200

    added feature getAllAuthors

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,3 +1,13 @@
 import dbPool from '../db/dbConnection.js';
 
-export {};
+const getAllAuthors = async (req, res) => {
+    try {
+      const { rows } = await dbPool.query('SELECT * FROM authors;');
+      console.log(rows);
+      return res.json(rows);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+export { getAllAuthors };
