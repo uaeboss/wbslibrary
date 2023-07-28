eeb6d3db29079fd3d955dc2994d2dd3239144bff
uaeboss <jeskemal@gmail.com>
2023-07-28 13:47:51 +0200

    feature createAuthor added

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -10,4 +10,23 @@ const getAllAuthors = async (req, res) => {
     }
   };
 
-export { getAllAuthors };
+  const createAuthor = async (req, res) => {
+    try {
+      const { first_name, last_name, about, image_url } = req.body;
+      if (!first_name || !last_name)
+        return res.status(400).json({ error: 'Name is required to add a new Author!' });
+  
+      const {
+        rows: [newAuthor],
+      } = await dbPool.query(
+        'INSERT INTO authors (first_name, last_name, about, image_url) VALUES ($1, $2, $3, $4) RETURNING *;',
+        [first_name, last_name, about, image_url]
+      );
+  
+      return res.json(newAuthor);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+export { getAllAuthors, createAuthor };
